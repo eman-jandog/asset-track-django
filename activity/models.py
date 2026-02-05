bab1b5530413476f710f41728b1707a7ffd40997
Name-San <eman.jandog@gmail.com>
2026-02-05 18:54:06 +0800

    design models for activity

--- activity/models.py
+++ activity/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Activity(models.Model):
+    ACTIVITY_TYPES = [
+        ("asset", "Asset"),
+        ("order", "Order"),
+        ("staff", "Staff"),
+        ("system", "System")
+    ]
+
+    type = models.CharField(
+        max_length=20,
+        choices=ACTIVITY_TYPES
+    )
+    
+    action = models.CharField(
+        max_length=100
+    )
+
+    message = models.TextField()
+
+    related_object_id = models.PositiveIntegerField(
+        null=True,
+        blank=True
+    )
+
+    related_object_type = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+
+    create_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return f'self.message'
--- inventory/settings.py
+++ inventory/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'user',
+    'activity',
     'crispy_forms',
     "crispy_bootstrap5",
     "crispy_tailwind",
