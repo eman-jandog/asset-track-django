92df1070d78f0b26b0f7231dbb8dd527f972eb00
Name-San <eman.jandog@gmail.com>
2026-02-10 18:46:02 +0800

    create signals.py in dashboard app

--- activity/services.py
+++ activity/services.py
@@ -1,9 +1,9 @@
 from .models import Activity
 
-
 def log_activity(
     *,
     type: str,
+    msg: str,
     action: str,
     message: str,
     user=None,
@@ -12,8 +12,8 @@ def log_activity(
     Activity.objects.create(
         type=type,
         action=action,
-        message=message,
+        message=msg,
         user=user,
         related_object_id=getattr(obj, "id", None),
-        related_object_type=obj.__class__.__name__ if obj else None,
+        related_object_type=obj.__class__.__name__ if obj else None
     )
---
+++ dashboard/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from activity.services import log_activity
+from .models import Asset
+
+@receiver(post_save, sender=Asset)
+def asset_created(sender, request, instance, created, **kwargs):
+    if not created:
+        return
+    
+    log_activity(
+        type="asset",
+        action="created",
+        message=f"New asset added {instance}",
+        user=request.user,
+        obj=instance
+    )
