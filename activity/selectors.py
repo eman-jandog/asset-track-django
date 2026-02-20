5e4f2e326f057e5f6472f690e8f8d2d6cbbf78c5
Name-San <eman.jandog@gmail.com>
2026-02-20 18:20:19 +0800

    fix: selectors.py to relative time since

--- activity/selectors.py
+++ activity/selectors.py
@@ -1,21 +1,18 @@
 from django.db.models import F, ExpressionWrapper, DurationField
 from django.db.models.functions import Now
+from django.utils.timezone import now
 from django.shortcuts import get_object_or_404
 from datetime import timedelta
 from .models import Activity
 
 def get_recent_activities(limit=5, user=None):
-    activities_qs = activities_qs.objects.all()[:limit]
 
-    if activities_qs is None:
+    activities = Activity.objects.all()[:limit]
+
+    if activities is None:
         return
-    
-    activities_qs = activities_qs.annotate(
-        last_activity=ExpressionWrapper(
-                Now() - F("created_at"), 
-                output_field=DurationField()
-            )
-        )
-    activities = activities_qs.values("action", "message", "user", "related_object_name", "related_object_serial")
+
+    for activity in activities:
+        activity.time_since = now() - activity.created_at
 
     return activities
